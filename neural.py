ad71d699838ae7db8663ac8c6545a926ac6ea487
Nurlan Imanov <nurlan.imanov.2020@mail.ru>
2019-11-15 23:16:12 +0400

    Novomber 16 23:16

--- neural.py
+++ neural.py
@@ -1,5 +1,13 @@
 import numpy as np
 import pandas as pd
 
-iris = pd.read_csv("iris_num.csv")
-iris
+iris = pd.read_csv('/home/nurlan/Desktop/Artificial Neural Network(ANN)/iris_num .data')
+
+
+iris.sample(frac = 1).reset_index(drop = False)
+
+
+#This function helps me to shuffle my data in ordet to get rid of overfitting 
+def Shuffle(data):
+    data = data.sample(frac = 1).reset_index(drop = True)
+    
